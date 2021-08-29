879dda69c06f1f940ad38d786e510d1e3e3ec5b0
Munagapati <MUNAGAL@nationwide.com>
2021-08-29 09:51:20 -0400

    changing text on feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello Lokesh...! This is changed...!!!
+          Edit <code>src/App.js</code> and save to reload. Hello Lokesh...! This is changed on feature branch....!!!
         </p>
         <a
           className="App-link"
